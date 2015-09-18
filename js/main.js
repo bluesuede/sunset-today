ddc65af851cf00ec609296e2d0c0cebbf8bc585d
Filip Zalamans <filip.zalamans@gmail.com>
2015-09-18 14:00:15 +1000

    Add sunrise and sunset, depends on what time user visits

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
         htmlElement = document.getElementById("html-element"),
         apiAnswer,
         presentationText = '',
-        googleApiAnswer;
+        googleApiAnswer,
+        coordinates;
 
     // Initializes the application
     function init() {
@@ -23,17 +24,8 @@
      */
     function geolocationSuccess( pos ) {
 
-        var crd = pos.coords,
-            request = new XMLHttpRequest();
-
-        // Do GET request towards sunset API, call function sunsetListener when data is loaded
-        // This is to get time of sunset
-        request.addEventListener("load", function () {
-            sunsetListener(this, crd);
-        }, false);
-        request.open("GET", "http://api.sunrise-sunset.org/json?lat=" + crd.latitude + "&lng=" + crd.longitude + "&date=today");
-        request.send();
-
+        coordinates = pos.coords;
+        callSunriseSunsetAPI();
     }
 
     /**
@@ -43,25 +35,68 @@
     function geolocationError() {
         alert("You have to give permission to access your location if you want this website to work.");
     }
+    
+    /**
+     * Do GET request to API for sunrise and sunset
+     *
+     * @params string, will do GET request today if string is "today" and tomorrow if string is "tomorrow"
+     *
+     */
+    function callSunriseSunsetAPI(todayOrTomorrow) {
+      
+      var date;
+      
+      // If function is called with param string "tomorrow", variable date will be the date of tomorrow
+      if(todayOrTomorrow === "tomorrow") {
+        date = (today.getDate() + 1);
+      }
+      else {
+        date = today.getDate();
+      }
+      
+      var request = new XMLHttpRequest(),
+          formattedDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + date;
+      
+      // Do GET request towards sunset API, call function sunsetListener when data is loaded
+      // This GET request is to get time of sunrise and sunset
+      if(todayOrTomorrow) {
+        request.addEventListener("load", sunriseListener);
+      }
+      else {
+        request.addEventListener("load", sunriseSunsetListener);
+      }
+      request.open("GET", "http://api.sunrise-sunset.org/json?lat=" + coordinates.latitude + "&lng=" + coordinates.longitude + "&date=" + formattedDate + "");
+      request.send();
+    }
+    
+    function sunriseListener() {      
+      
+        // Parse response data from sunrise-sunset to JSON object
+        apiAnswer = JSON.parse(this.responseText);
+        
+        var timeForSunrise = formatOutputStrings("sunrise");
+        
+        sunriseAnimations();
+        displayInfoToUser(timeForSunrise);
+    }
 
     /**
      * Callback function when data from sunrise-sunset API is loaded
      *
-     * @param object response (the data recieved from the GET request to sunrise-sunset API),
-     * @param object crd (coordinates of user)
+     * this = response from get request in callSunriseSunsetAPI()
      *
      */
-    function sunsetListener(response, crd) {
+    function sunriseSunsetListener() {
 
         var requestGoogle = new XMLHttpRequest; // Used to call GET from Google
 
         // Parse response data from sunrise-sunset to JSON object
-        apiAnswer = JSON.parse(response.responseText);
+        apiAnswer = JSON.parse(this.responseText);
 
         // Do GET request towards Google API, call function googleListener when data is loaded
-        // This is to get address/specified location of user
+        // This GET request is to get address/specified location of user
         requestGoogle.addEventListener("load", googleListener);
-        requestGoogle.open("GET", "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + crd.latitude + "," + crd.longitude + "&sensor=true");
+        requestGoogle.open("GET", "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinates.latitude + "," + coordinates.longitude + "&sensor=true");
         requestGoogle.send();
 
     }
@@ -70,69 +105,118 @@
      * Callback function when data from Google API is loaded
      *
      */
-     function googleListener() {
+    function googleListener() {
 
         // Parse response data from Google to JSON Object
         googleApiAnswer = JSON.parse(this.responseText);
 
-        // Both API results are in global variables, call the formatting function
-        formatOutputStrings();
+        // Both API results are in global variables, call the outputting function
+        outputToUser();
     };
+    
+    /**
+     * Displays sunset time and does animations if sunset hasnt happened yet
+     * If time is after time of sunset, calls function callSunriseSunsetAPI() to find sunrise time of tomorrow
+     *
+     */
+    function outputToUser() {
+      var timeForSunset,
+          timeForSunsetFour,
+          timeForSunrise,
+          timeForSunriseFour,
+          nowString,
+          nowFour,
+          todayOrTomorrow;
+        
+      // Get formatted adress in response from Google
+      googleApiAnswer = googleApiAnswer.results[2].formatted_address;
+      
+      // Get time of sunset and sunrise for today in format "00:00:00 AM/PM"
+      timeForSunrise  = formatOutputStrings("sunrise");
+      timeForSunset   = formatOutputStrings("sunset");
+      
+      // Make sure nowString has four digits
+      nowString = today.getHours() + ':';
+      if(today.getMinutes() < 10) {
+        nowString += "0" + today.getMinutes();
+      }
+      else {
+        nowString += today.getMinutes();
+      }
+      
+      ///////////////////////////////////////
+      // CAN BE USED FOR TESTING WHEN USER IS ON SITE
+      //nowString = "05:00";
+      ///////////////////////////////////////
+      
+      // Make all times in to string format "0000"
+      sunriseFour = convertTimeToFourDigitString(timeForSunrise);
+      sunsetFour  = convertTimeToFourDigitString(timeForSunset);
+      nowFour     = convertTimeToFourDigitString(nowString);
+      
+      // If time is between sunset and midnight
+      if(nowFour > sunsetFour) {
+        todayOrTomorrow = "tomorrow";
+        callSunriseSunsetAPI(todayOrTomorrow);
+      }
+      // If time is between midnight and sunrise
+      else if(nowFour < sunriseFour) {
+        sunriseAnimations();
+        displayInfoToUser(timeForSunrise);
+      }
+      // If time is between sunrise and sunset
+      else {
+        // Add classes to elements to do css animations
+        sun.className = "do-move-down";
+        htmlElement.className = "do-sunset-color-fade";
+        presentationText = "The sun will set ";
+        displayInfoToUser(timeForSunset); // Changes text in elements
+      }
+      
+    }
 
     /**
      * Format the API results in to strings to be displayed for user
-     * Using global variables googleApiAnswer and apiAnswer
+     * Using global variable apiAnswer
+     *
+     * @param string, "sunrise" if you want sunset results and "sunset" if you want sunset
+     *
+     * @return string, containing time in format "00:00 AM/PM"
      *
      */
-     function formatOutputStrings() {
-        var now = new Date(),
-            nowString = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds(),
-            timeInMinutes,
+    function formatOutputStrings(string) {
+        var timeInMinutes,
             amOrPm,
             timezoneOffset = today.getTimezoneOffset(),
             timezoneAsString,
             hours,
             minutes,
-            timeForOutput;
-
-        // Get formatted adress in response from Google
-        googleApiAnswer = googleApiAnswer.results[2].formatted_address;
-
-        // Time of sunset, format 00:00:00 AM/PM
-        apiAnswer = apiAnswer.results.sunset;
-
-        // Save either string "AM" or "PM" from api answer
-        amOrPm = apiAnswer.substr(apiAnswer.length - 2);
-
-        if( nowString > convertTo24Hour(apiAnswer) ) {
-
-            // Add classes to elements to do css animations
-            sun.className = "do-move-up";
-            htmlElement.className = "do-sunrise-color-fade";
-            presentationText = "The sun will rise ";
-
-        } else {
-
-            // Add classes to elements to do css animations
-            sun.className = "do-move-down";
-            htmlElement.className = "do-sunset-color-fade";
-            presentationText = "The sun will set ";
-
+            timeForOutput,
+            timeToWorkWith;
+        
+        if(string === "sunset") {
+          timeToWorkWith = apiAnswer.results.sunset; // Time of sunset, format 00:00:00 AM/PM
+        }
+        else if(string === "sunrise") {
+          timeToWorkWith = apiAnswer.results.sunrise; // Time of sunrise, format 00:00:00 AM/PM
         }
 
+        // Save either string "AM" or "PM" from api answer
+        amOrPm = timeToWorkWith.substr(timeToWorkWith.length - 2);
+
         // Remove last three characters (space and AM or PM)
-        apiAnswer = apiAnswer.substring(0, apiAnswer.length - 3);
+        timeToWorkWith = timeToWorkWith.substring(0, timeToWorkWith.length - 3);
 
         // Make api answer in to array and add the hours and minutes together in form of minutes
-        apiAnswer = apiAnswer.split(":");
+        timeToWorkWith = timeToWorkWith.split(":");
 
         // If the seconds are greater than 30, round up an extra minute
-        if (parseInt(apiAnswer[2]) > 29) {
-            apiAnswer[1] = parseInt(apiAnswer[1]) + 1;
+        if (parseInt(timeToWorkWith[2]) > 29) {
+            timeToWorkWith[1] = parseInt(timeToWorkWith[1]) + 1;
         }
 
         // Get the total amount of minutes of time given from API
-        timeInMinutes = parseInt(apiAnswer[0] * 60) + parseInt(apiAnswer[1]);
+        timeInMinutes = parseInt(timeToWorkWith[0] * 60) + parseInt(timeToWorkWith[1]);
 
         // If api answer was PM, add 720 minutes (12 hours) to timeInMinutes
         if (amOrPm === "PM") {
@@ -154,6 +238,11 @@
             // Add the positive timezone offset to the time in minutes
             timeInMinutes += timezoneOffset;
         }
+        
+        // If timeInMinutes is more than 24 hrs, remove 24 hrs of time in minutes
+        if(timeInMinutes > 1440) {
+          timeInMinutes = (timeInMinutes - 1440);
+        }
 
         // Convert timeInMinutes back to 12 hour clock
         hours = parseInt(timeInMinutes / 60);
@@ -174,35 +263,93 @@
             timeForOutput = hours + ":" + minutes + " AM";
         }
 
-        displaySunsetTimeLocation(timeForOutput, googleApiAnswer);
+        return timeForOutput;
     };
+    
+    /**
+     * Does animations for sunrise and changes text
+     *
+     */
+    function sunriseAnimations() {
+      // Add classes to elements to do css animations
+      sun.className = "do-move-up";
+      htmlElement.className = "do-sunrise-color-fade";
+      presentationText = "The sun will rise";
+    }
 
     /**
      * Change elements to display the time and location in the proper formats
      *
-     * @param time string
-     * @param location string
+     * @param string
      *
      */
-     function displaySunsetTimeLocation(time, location) {
-        var textContainer = document.getElementById("text-in-box"),
-            timeParagraph = document.getElementById("loader-and-result"),
-            locationParagraph = document.getElementById("city-country");
+    function displayInfoToUser(time) {
+      var textContainer = document.getElementById("text-in-box"),
+          timeParagraph = document.getElementById("loader-and-result"),
+          locationParagraph = document.getElementById("city-country");
 
-        textContainer.textContent =presentationText;
-        timeParagraph.textContent = time;
-        locationParagraph.textContent = location;
+      textContainer.textContent = presentationText;
+      timeParagraph.textContent = time;
+      locationParagraph.textContent = googleApiAnswer;
     };
-
-    function convertTo24Hour(time) {
-        var hours = parseInt(time.substr(0, 2));
-        if(time.indexOf('AM') != -1 && hours == 12) {
-            time = time.replace('12', '0');
-        }
-        if(time.indexOf('PM')  != -1 && hours < 12) {
-            time = time.replace(hours, (hours + 12));
-        }
-        return time.replace(/(AM|PM)/, '');
+    
+    /**
+     * Converts a string with time info in to a four digit string
+     *
+     * @params string, formats "00:00" and "00:00 AM/PM" tested
+     *
+     * @return string, format "0000"
+     *
+     */
+    function convertTimeToFourDigitString(time) {
+      var amOrPm,
+          hours,
+          minutes;
+      
+      if(time.length > 5) {
+        // Set variable to either AM or PM depending on last two characters of string
+        amOrPm = time.substr(time.length - 2);
+        
+        // Remove last three characters (space and AM or PM)
+        time = time.substring(0, time.length - 3);
+      }
+      
+      // Make in to array with hours and minutes being one value each
+      time = time.split(":");
+      
+      hours = parseInt(time[0]);
+      
+      if(amOrPm === "PM") {
+        hours = hours + 12;
+      }
+      
+      hours = addAZeroIfLessThanTen(hours);
+      
+      minutes = parseInt(time[1]);
+      minutes = addAZeroIfLessThanTen(minutes);
+      
+      time = hours + "" + minutes;
+      
+      return time;
+      
+    }
+    
+    /**
+     * Takes an integer or string and checks it is less than ten, concatenates number to a "0" if true
+     *
+     * @params string/integer
+     *
+     * @return string/integer
+     *
+     */
+    function addAZeroIfLessThanTen(number) {
+      number = parseInt(number);
+      if(number < 10) {
+        return "0" + number;
+      }
+      else {
+        return number;
+      }
     }
 
     init();
